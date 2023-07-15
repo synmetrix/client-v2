f6f9facd23c7c3ef5a8ef7f495242ba80965de96
Sedrak Adoyan <set.adoyan@gmail.com>
2023-07-15 16:13:52 +0400

    feat: add GeneralInfoForm component

---
+++ src/components/GeneralInfoForm/index.stories.tsx
@@ -0,0 +1,26 @@
+import RootLayout from "@/layouts/RootLayout";
+
+import GeneralInfoForm from ".";
+
+import type { StoryFn, Meta } from "@storybook/react";
+
+export default {
+  title: "Components/GeneralInfoForm",
+  component: GeneralInfoForm,
+} as Meta<typeof GeneralInfoForm>;
+
+const Template: StoryFn<typeof GeneralInfoForm> = (args) => (
+  <RootLayout>
+    <GeneralInfoForm {...args} />
+  </RootLayout>
+);
+
+export const Default = Template.bind({});
+
+Default.args = {
+  initialValue: {
+    email: "email@email.ge",
+    fullName: "Full Name",
+  },
+  onSubmit: console.log,
+};

d8ad69d153b05d292a172f959fe73cd0abd88a5c
Sedrak Adoyan <set.adoyan@gmail.com>
2023-07-18 08:55:28 +0400

    fix: remove lodash

--- src/utils/helpers/createRoleOptions.ts
+++ src/utils/helpers/createRoleOptions.ts
@@ -1,11 +1,11 @@
-import lodash from "lodash";
-
 import type { Role } from "@/types/team";
+
+import { capitalize } from "./capitalize";
 
 export const createRoleOptions = (role: typeof Role) =>
   Object.keys(role)
     .filter((v: string | number) => isNaN(v as number))
     .map((v) => ({
       value: v,
-      label: lodash.capitalize(v),
+      label: capitalize(v),
     }));

4d2567bdbd9031dea79fa5705b1be46e40500595
Sedrak Adoyan <set.adoyan@gmail.com>
2023-07-29 17:12:30 +0400

    feat: add Input types in storybook

--- src/components/Input/index.stories.tsx
+++ src/components/Input/index.stories.tsx
@@ -1,3 +1,4 @@
+import { Form } from "antd";
 import { useForm } from "react-hook-form";
 
 import RootLayout from "@/layouts/RootLayout";
@@ -9,20 +10,56 @@ import type { StoryFn, Meta } from "@storybook/react";
 export default {
   title: "Components/Basic/Input",
   component: Input,
+  argTypes: {
+    fieldType: {
+      type: "string",
+      description: "Field type",
+      defaultValue: "text",
+      options: [
+        "text",
+        "select",
+        "textarea",
+        "password",
+        "file",
+        "radio",
+        "checkbox",
+      ],
+      control: {
+        type: "radio",
+      },
+    },
+  },
 } as Meta<typeof Input>;
 
-interface Form {
+interface FormType {
   test: string;
 }
 
-const Template: StoryFn<typeof Input> = () => {
-  const { control } = useForm<Form>();
+const Template: StoryFn<typeof Input> = (args) => {
+  const { control } = useForm<FormType>();
 
   return (
     <RootLayout>
-      <Input<Form> name="test" defaultValue="test" control={control} />
+      <Form>
+        <Input<FormType> {...args} name="test" control={control} />
+      </Form>
     </RootLayout>
   );
 };
 
 export const Default = Template.bind({});
+Default.args = {
+  fieldType: "text",
+  label: "test",
+  placeholder: "test",
+  options: [
+    {
+      label: "option1",
+      value: "option1",
+    },
+    {
+      label: "option2",
+      value: "option2",
+    },
+  ],
+};

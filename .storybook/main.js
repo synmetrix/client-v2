e79ef5fd796fbcaa58c77d0f9f7174af9bda7af1
Sedrak Adoyan <set.adoyan@gmail.com>
2023-08-31 02:05:19 +0400

    fix: CodeEditor navbar padding

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,8 @@
+// .storybook/main.js
+const {
+  getCodeEditorStaticDirs,
+} = require("storybook-addon-code-editor/getStaticDirs");
+
 module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
@@ -5,6 +10,7 @@ module.exports = {
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
     "@storybook/addon-mdx-gfm",
+    "storybook-addon-code-editor",
   ],
   framework: {
     name: "@storybook/react-vite",
@@ -16,4 +22,6 @@ module.exports = {
   docs: {
     autodocs: true,
   },
+  staticDirs: [...getCodeEditorStaticDirs()],
+  core: { builder: "@storybook/builder-vite" },
 };

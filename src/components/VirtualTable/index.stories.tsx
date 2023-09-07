8f3a3c2f868486d1bf1be29b84c2e95ddc879b6b
Sedrak Adoyan <set.adoyan@gmail.com>
2023-09-08 01:06:49 +0400

    feat: add VirtualTable props

---
+++ src/components/VirtualTable/index.stories.tsx
@@ -0,0 +1,18 @@
+import RootLayout from "@/layouts/RootLayout";
+
+import VirtualTable from ".";
+
+import type { StoryFn, Meta } from "@storybook/react";
+
+export default {
+  title: "Components/Basic/VirtualTable",
+  component: VirtualTable,
+} as Meta<typeof VirtualTable>;
+
+const Template: StoryFn<typeof VirtualTable> = (args) => (
+  <RootLayout>
+    <VirtualTable {...args} />
+  </RootLayout>
+);
+
+export const Default = Template.bind({});

a2e2c21ed24608a168777e78bb585d326ffa518f
Sedrak Adoyan <set.adoyan@gmail.com>
2023-09-12 00:58:04 +0400

    feat: add PopoverButton component

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,7 @@
 // import * as path from 'path';
+import path from "path";
+import fs from "fs";
+
 import react from "@vitejs/plugin-react";
 import vitApp from "@vitjs/vit";
 import { visualizer } from "rollup-plugin-visualizer";
@@ -11,6 +14,24 @@ import svgx from "@svgx/vite-plugin-react";
 import monacoEditorPlugin from "vite-plugin-monaco-editor";
 
 import routes from "./config/routes";
+
+const WRONG_CODE = `import { bpfrpt_proptype_WindowScroller } from "../WindowScroller.js";`;
+export function reactVirtualized() {
+  return {
+    name: "my:react-virtualized",
+    configResolved() {
+      const file = require
+        .resolve("react-virtualized")
+        .replace(
+          path.join("dist", "commonjs", "index.js"),
+          path.join("dist", "es", "WindowScroller", "utils", "onScroll.js")
+        );
+      const code = fs.readFileSync(file, "utf-8");
+      const modified = code.replace(WRONG_CODE, "");
+      fs.writeFileSync(file, modified);
+    },
+  };
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -57,6 +78,7 @@ export default defineConfig({
     visualizer(),
     svgx(),
     monacoEditorPlugin({}),
+    reactVirtualized(),
   ],
   server: {
     // open: true,

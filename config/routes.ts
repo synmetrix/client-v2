2bf25ab22f03c35da3333e8657543e075b2cba35
Ivan Fokeev <vanya@vanya.cc>
2023-09-25 14:28:02 +0300

    fix(routes): remove BasicLayout duplication

--- config/routes.ts
+++ config/routes.ts
@@ -8,7 +8,6 @@ export default [
         routes: [
           {
             path: "/",
-            component: "./layouts/BasicLayout",
             routes: [
               {
                 path: "/signup",

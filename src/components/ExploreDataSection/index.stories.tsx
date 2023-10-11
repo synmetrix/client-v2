7b788c2761db9676d840a32b134db825a5b6ecdd
Sedrak Adoyan <set.adoyan@gmail.com>
2023-10-11 17:28:13 +0400

    fix: ExploreDataSection story

--- src/components/ExploreDataSection/index.stories.tsx
+++ src/components/ExploreDataSection/index.stories.tsx
@@ -1,5 +1,6 @@
 import RootLayout from "@/layouts/RootLayout";
 import { dataSectionProps } from "@/mocks/explore";
+import { getTitle } from "@/utils/helpers/getTitles";
 
 import ExploreDataSection from ".";
 
@@ -11,13 +12,41 @@ export default {
 } as Meta<typeof ExploreDataSection>;
 
 const Template: StoryFn<typeof ExploreDataSection> = (args) => {
+  const [settings, setSettings] = useState({
+    hideIndexColumn: false,
+    hideCubeNames: false,
+  });
+
+  const [columns, setColumns] = useState(args.queryState.columns);
+
   const [isActive, setIsActive] = useState(false);
+
+  const onQueryChange = (type: string, value: boolean) => {
+    if (settings[type as keyof typeof settings] === value) return;
+
+    setSettings((prev) => ({ ...prev, [type]: value }));
+  };
+
+  useEffect(() => {
+    setColumns(
+      args.queryState.columns.map((c) => {
+        const header = c["Header" as keyof typeof c] as string;
+        const newHeader = settings.hideCubeNames
+          ? header.split(" ")[1]
+          : header;
+        return { ...c, Header: newHeader };
+      })
+    );
+  }, [args.queryState.columns, args.queryState.rows, settings]);
+
   return (
     <RootLayout>
       <ExploreDataSection
         {...args}
         isActive={isActive}
         onToggleSection={() => setIsActive((prev) => !prev)}
+        onQueryChange={onQueryChange}
+        queryState={{ ...args.queryState, settings, columns }}
       />
     </RootLayout>
   );

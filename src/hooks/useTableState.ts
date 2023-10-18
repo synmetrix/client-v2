43ca9d55868745a7587b54bae14129afbe90d5bd
Sedrak Adoyan <set.adoyan@gmail.com>
2023-10-18 15:44:01 +0400

    feat: add useTableState hook

---
+++ src/hooks/useTableState.ts
@@ -0,0 +1,56 @@
+import { useEffect } from "react";
+import { useSetState } from "ahooks";
+import { TablePaginationConfig } from "antd";
+
+const defaultPageSize = 10;
+
+interface Props {
+  customPageSize?: number;
+}
+
+interface State {
+  allTags: any[];
+  loading: boolean;
+  selectedTags: any[];
+  paginationVars: {
+    limit: number;
+    offset: number;
+  };
+  pageSize: number;
+  currentPage: number;
+}
+
+export default ({ customPageSize }: Props) => {
+  const pageSize = customPageSize || defaultPageSize;
+
+  const [tableState, updateTableState] = useSetState<State>({
+    allTags: [],
+    loading: false,
+    selectedTags: [],
+    currentPage: 1,
+    paginationVars: {
+      limit: pageSize,
+      offset: 0,
+    },
+    pageSize,
+  });
+
+  useEffect(() => {
+    updateTableState({
+      paginationVars: {
+        limit: pageSize,
+        offset: pageSize * (tableState.currentPage - 1),
+      },
+    });
+  }, [pageSize, tableState.currentPage, updateTableState]);
+
+  const onPageChange = ({ current }: TablePaginationConfig) => {
+    updateTableState({ currentPage: current || 1 });
+  };
+
+  return {
+    tableState,
+    onPageChange,
+    updateTableState,
+  };
+};

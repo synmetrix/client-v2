4700e0f79825bbc4c80bdcc2602450b6fb968c35
Nikita <gorland1314@list.ru>
2023-10-26 20:59:13 +0300

    fix(settings): Removed jerking during rendering (#57)

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -3,14 +3,18 @@ import styles from "./index.module.less";
 import type { FC, ReactNode } from "react";
 
 interface SidebarProps {
+  icon?: ReactNode;
   title: ReactNode;
   children: ReactNode;
 }
 
-const Sidebar: FC<SidebarProps> = ({ title, children }) => {
+const Sidebar: FC<SidebarProps> = ({ icon, title, children }) => {
   return (
     <div className={styles.wrapper}>
-      <div className={styles.header}>{title}</div>
+      <div className={styles.header}>
+        {icon && icon}
+        <div>{title}</div>
+      </div>
       <div className={styles.body}>{children}</div>
     </div>
   );
---
+++ src/layouts/SettingsLayout/index.tsx
@@ -0,0 +1,37 @@
+import { useTranslation } from "react-i18next";
+
+import SidebarLayout from "@/layouts/SidebarLayout";
+import SidebarMenu from "@/components/SidebarMenu";
+import { settingsMenuItems } from "@/mocks/sidebarMenu";
+import type { Location } from "@/hooks/useLocation";
+
+import Icon from "@/assets/settings-active.svg";
+
+import type { ReactNode } from "react";
+
+export type SidebarLayoutProps = {
+  title?: string;
+  children: ReactNode;
+  location: Location;
+};
+
+const SettingsLayout: React.FC<SidebarLayoutProps> = ({
+  title,
+  children,
+  location,
+}) => {
+  const { t } = useTranslation(["pages"]);
+  const titleKey = (location?.pathname || "").split("/")?.[2];
+
+  return (
+    <SidebarLayout
+      icon={<Icon />}
+      title={title || t(`pages:settings.${titleKey}`)}
+      items={<SidebarMenu items={settingsMenuItems} />}
+    >
+      {children}
+    </SidebarLayout>
+  );
+};
+
+export default SettingsLayout;

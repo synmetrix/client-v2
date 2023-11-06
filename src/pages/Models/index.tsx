662f87859d6f3f0cbb0d753f383530a60ba0e1ba
Sedrak Adoyan <set.adoyan@gmail.com>
2023-11-06 15:59:31 +0400

    refactor: add helpers

--- src/pages/Models/index.tsx
+++ src/pages/Models/index.tsx
@@ -25,6 +25,8 @@ import useCheckResponse from "@/hooks/useCheckResponse";
 import usePermissions from "@/hooks/usePermissions";
 import equals from "@/utils/helpers/equals";
 import calcChecksum from "@/utils/helpers/dataschemasChecksum";
+import getTables from "@/utils/helpers/getTables";
+import getCurrentBranch from "@/utils/helpers/getCurrentBranch";
 import type { Branch, DataSourceInfo, Schema } from "@/types/dataSource";
 import type { Dataschema } from "@/types/dataschema";
 import type { Version } from "@/types/version";
@@ -237,29 +239,6 @@ export const Models: React.FC<ModelsProps> = ({
 };
 
 const reservedSlugs = ["sqlrunner", "genschema", "docs"];
-
-const getTables = (obj: object, prefix: string = "") => {
-  return Object.entries(obj).reduce((acc: { name: string }[], [key, value]) => {
-    let result = acc;
-    const newPath = prefix ? `${prefix}.${key}` : key;
-
-    if (value === true) {
-      const lastSlashIndex = newPath.lastIndexOf(".");
-      const formattedPath = `${newPath.slice(
-        0,
-        lastSlashIndex
-      )}.${newPath.slice(lastSlashIndex + 1)}`;
-      result.push({ name: formattedPath });
-    }
-
-    if (typeof value === "object") {
-      const childResults = getTables(value, newPath);
-      result = acc.concat(childResults);
-    }
-
-    return result;
-  }, []);
-};
 
 const ModelsWrapper: React.FC = () => {
   const { t } = useTranslation(["models", "common"]);
@@ -861,16 +840,5 @@ const ModelsWrapper: React.FC = () => {
     />
   );
 };
-
-function getCurrentBranch(dataSource: DataSourceInfo | null) {
-  if (!dataSource) return null;
-
-  const branchFromLocalStorage = localStorage.getItem(
-    `${dataSource.id}:currentBranch`
-  );
-  return branchFromLocalStorage
-    ? JSON.parse(branchFromLocalStorage)
-    : dataSource.branch.id;
-}
 
 export default ModelsWrapper;
---
+++ src/utils/helpers/getCurrentBranch.ts
@@ -0,0 +1,12 @@
+import type { DataSourceInfo } from "@/types/dataSource";
+
+export default function getCurrentBranch(dataSource: DataSourceInfo | null) {
+  if (!dataSource) return null;
+
+  const branchFromLocalStorage = localStorage.getItem(
+    `${dataSource.id}:currentBranch`
+  );
+  return branchFromLocalStorage
+    ? JSON.parse(branchFromLocalStorage)
+    : dataSource.branch.id;
+}

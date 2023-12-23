35628d86e72cf7071edb474916fd8daede72e129
Sedrak Adoyan <set.adoyan@gmail.com>
2023-12-23 16:58:47 +0400

    fix: tests setup (#122)

--- tests.setup.ts
+++ tests.setup.ts
@@ -1,9 +1,31 @@
-import { expect, afterEach } from "vitest";
+import { expect, afterEach, vi } from "vitest";
 import { cleanup } from "@testing-library/react";
 import * as matchers from "@testing-library/jest-dom/matchers";
+import "@testing-library/jest-dom";
 
 // extends Vitest's expect method with methods from react-testing-library
 expect.extend(matchers);
+Object.defineProperty(window, "matchMedia", {
+  writable: true,
+  value: vi.fn().mockImplementation((query) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: vi.fn(), // Deprecated
+    removeListener: vi.fn(), // Deprecated
+    addEventListener: vi.fn(),
+    removeEventListener: vi.fn(),
+    dispatchEvent: vi.fn(),
+  })),
+});
+
+Object.defineProperty(window, "computedStyle", {
+  writable: true,
+  value: vi.fn().mockImplementation(() => {}),
+});
+
+const { getComputedStyle } = window;
+window.getComputedStyle = (elt) => getComputedStyle(elt);
 
 // runs a cleanup after each test case (e.g. clearing jsdom)
 afterEach(() => {

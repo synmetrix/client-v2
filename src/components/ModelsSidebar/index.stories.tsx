637b8b81fa6bac2f9f445c9517c067655f1cf1f5
Nikita <gorland1314@list.ru>
2024-01-07 00:28:25 +0300

    fixes

--- src/components/ModelsSidebar/index.stories.tsx
+++ src/components/ModelsSidebar/index.stories.tsx
@@ -1,6 +1,7 @@
 import RootLayout from "@/layouts/RootLayout";
 import { branchesMock } from "@/mocks/branches";
 import { dataschemasMock } from "@/mocks/dataschemas";
+import { dataSourcesMock } from "@/mocks/dataSources";
 
 import ModelsSidebar from ".";
 
@@ -24,6 +25,7 @@ Default.args = {
   version: "34f34fjn5g4j5g95jf3ef4t4ht5",
   docs: "https://google.com",
   files: dataschemasMock,
+  dataSources: dataSourcesMock,
   onCreateFile: console.log,
   onSelectFile: console.log,
 };

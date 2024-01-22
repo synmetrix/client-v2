728b9b346ab2b48899b4455a6915666d43a0c01f
Ivan Fokeev <vanya@vanya.cc>
2024-01-22 17:18:40 +0300

    fix: proxy

--- vite.config.ts
+++ vite.config.ts
@@ -86,29 +86,7 @@ export default defineConfig({
   server: {
     port: 8000,
     proxy: {
-      "/api/v1": {
-        forward: "https://localhost:4000",
-        xfwd: true,
-        changeOrigin: true,
-        secure: false,
-        ssl: false,
-        ws: true,
-        configure: (proxy, _options) => {
-          proxy.on("error", (err, _req, _res) => {
-            console.log("proxy error", err);
-          });
-          proxy.on("proxyReq", (proxyReq, req, _res) => {
-            console.log("Sending Request to the Target:", req.method, req.url);
-          });
-          proxy.on("proxyRes", (proxyRes, req, _res) => {
-            console.log(
-              "Received Response from the Target:",
-              proxyRes.statusCode,
-              req.url
-            );
-          });
-        },
-      },
+      "/api/v1": "http://localhost:4000",
     },
   },
   resolve: {
